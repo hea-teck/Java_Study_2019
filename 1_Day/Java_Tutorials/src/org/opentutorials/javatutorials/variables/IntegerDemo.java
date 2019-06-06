85505a8e5602748bc3e6e695f7c827da24ca9de0
huitaek <rkfgus90@naver.com>
2019-06-06 11:42:46 +0900

    Basic Number, String, Variables

---
+++ 1_Day/Java_Tutorials/src/org/opentutorials/javatutorials/variables/IntegerDemo.java
@@ -0,0 +1,17 @@
+package org.opentutorials.javatutorials.variables;
+
+public class IntegerDemo {
+
+	public static void main(String[] args) {
+
+		int a;
+		
+		a = 1;		
+		System.out.println(a+1);
+		
+		a = 2;		
+		System.out.println(a+2);
+		
+	}
+
+}
